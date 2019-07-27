ad23654f8f7cd90a162687d7aee0b553901fd506
Steven Hunal <stevehunal@gmail.com>
2019-07-27 23:01:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each_with_index(1) do |name|
+  end
 end
 
 def summon_captain_planet# code an argument here
