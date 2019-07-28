4ffcdb7c3848e3955894957f792d85358cb05e74
Steven Hunal <stevehunal@gmail.com>
2019-07-28 00:36:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,13 +13,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.map do |calls|
+  array.each do |calls|
     if calls.length > 4
      return true
     else
       return false
-   end
-    end
+  end
+ end
 end
 
 def find_the_cheese(array)
